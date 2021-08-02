e0987944e4cf4b2b911033231645d1ad5608dfdb
Oleg Kononov <kononov.itwork@gmail.com>
2021-08-03 02:16:05 +0300

    feat(ingridient calc): add ingredients calculation for coffee

--- src/com/machine/CoffeeMachine.java
+++ src/com/machine/CoffeeMachine.java
@@ -1,8 +1,34 @@
 package com.machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Write how many cups of coffee you will need:");
+
+        Scanner scanner = new Scanner(System.in);
+        int cups = scanner.nextInt();
+
+        final int WATER_PER_SERVING = 200;
+        final int MILK_PER_SERVING = 50;
+        final int BEANS_PER_SERVING = 15;
+
+        int waterTotal = cups * WATER_PER_SERVING;
+        int milkTotal = cups * MILK_PER_SERVING;
+        int beansTotal = cups * BEANS_PER_SERVING;
+
+        String resultMessage = String.format(
+            "For %d cups of coffee you will need:\n"
+                + "%d ml of water\n"
+                + "%d ml of milk\n"
+                + "%d g of coffee beans",
+            cups,
+            waterTotal,
+            milkTotal,
+            beansTotal
+        );
+
+        System.out.println(resultMessage);
     }
 }
